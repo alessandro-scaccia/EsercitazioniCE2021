5dd00acf7fb95fe091817be2d4ac100059201617
alessandro-scaccia <alex.scaccia99@gmail.com>
2020-12-10 15:44:23 +0100

    Revisione es15

--- es15.c
+++ es15.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-char *parola = "OVATTAVO";
+char *parola = "AOVATTAVO";
 
 
 bool ver_palindroma_iter(char *stringa)
@@ -14,16 +14,39 @@ bool ver_palindroma_iter(char *stringa)
 	for(i=0; i<n/2; i++) {
 		if(stringa[i] != stringa[n-1-i]) {
 			flag = false;
+			break;
 		}
 	}
 	return flag;
 }
 
 
+bool palindroma_aux(char a, char b, char stringa[])
+{
+	if(a == b)
+		return true;
+	if(stringa[a] != stringa[b])
+		return false;
+	if(a < b+1)
+		return palindroma_aux(a+1,b-1,stringa);
+}
+
+
+bool ver_palindroma_rec(char *stringa)
+{
+	int lung = strlen(stringa);
+	if(lung == 0)
+		return true;
+	else
+		return palindroma_aux(0,lung-1,stringa);
+}
+
+
 int main()
 {
 	bool verifica;
-	verifica = ver_palindroma_iter(parola);
+//	verifica = ver_palindroma_iter(parola);
+	verifica = ver_palindroma_rec(parola);
 	printf("La stringa e' palindroma: %c\n", (verifica == true) ? 'y' : 'n');
 	return 0;
 }
