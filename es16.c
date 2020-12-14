7e26627f13eec418223b93855c95cf3a854f0d01
alessandro-scaccia <alex.scaccia99@gmail.com>
2020-12-14 17:21:27 +0100

    es16 finito

--- es16.c
+++ es16.c
@@ -1,46 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct elemento{
-	int row;
-	int col;
-	int data;
-}
 
 struct lista {
-	struct elemento nodo;
+	int row;
+	int col;
+	int elemento;
 	struct lista *link;
-}
+};
 
-struct lista *rest_lista(int row, int col, int matrice)
-{	
-	int i, j; 				// variabili per iterare sulla matrice
+
+struct lista *esamina_matrice(int row, int col, int matrix[row][col])
+{
+	struct lista *head;
 	struct lista *ptr;
-	struct lista *head;			// puntatore testa della lista
-	head->next = NULL;			// lo inizializzo come puntatore nullo
-	head = (struct lista *)malloc(sizeof(struct lista));
-
-	for(i=0; i<row; ++i) {
-		for(j=0; i<col; ++j) {
-			
-			if(matrice[i][j] != 0) {
-				head->next = ptr;
+	int i, j;
+	ptr = malloc(sizeof(struct lista));
+	head = ptr;
+	
+	for(i=0; i<row; i++) {
+		for(j=0; j<col; j++) {
+			if(matrix[i][j] != 0) {
+				ptr->row = i;
+				ptr->col = j;
+				ptr->elemento = matrix[i][j];
+				ptr->link = malloc(sizeof(struct lista));
+				ptr = ptr->link;		
 			}
 		}
 	}
+	ptr->link = NULL;
+	return head;
+}
+
+
+void print_list(struct lista *ptr)
+{
+	printf("L: ");
+	while(ptr->link != NULL) {
+		printf("(%d,%d,%d)", ptr->elemento, ptr->row, ptr->col);
+		ptr = ptr->link;
+		if(ptr->link != NULL) {
+			printf(" -> ");
+		}
+	}
+	printf("\n");
 }
 
 
 int main()
-{
-	int row = 3;
-	int col = 3;
-	int matrice[row][col] = {{0,1,0},{7,0,0},{0,0,5}};
-	struct lista *ptr;
-
-	ptr = rest_lista(row,col,matrice);
-	
-	print_lista(ptr);
-
-	return 0;
+{	
+	int row = 4;
+	int col = 4;
+	int matrice[4][4] = {{0,0,0,0},{5,8,0,0},{0,0,3,0},{0,6,0,0}};
+	struct lista *ret_lista;
+	ret_lista = esamina_matrice(row,col,matrice);
+	print_list(ret_lista);
 }
